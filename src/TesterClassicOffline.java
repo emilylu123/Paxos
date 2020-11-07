2c67b7bbb329ac8c51c8f267cf8bb790c40b478d
Yingyao Lu <54159365+emilylu123@users.noreply.github.com>
2020-11-07 17:19:34 +1030

    final

--- src/TesterClassicOffline.java
+++ src/TesterClassicOffline.java
@@ -7,12 +7,13 @@
 // Assignment3: Paxos
 //=====================================
 
-public class TesterMultiNormal extends Election {
+public class TesterClassicOffline extends Election {
     public static void main(String[] args) {
         System.out.println("============= [ Test Description ] ===================");
-        System.out.println("Test 1 :: Classic Paxos - No failure");
+        System.out.println("Test 2 :: Classic Paxos - with failure (Offline)");
         System.out.println("1. Proposer: M1 - M3 proposes to all members at the same time;");
-        System.out.println("2. Acceptor: All members response immediately.");
+        System.out.println("2. Offline : M2 / M3 proposes and then goes offline;");
+        System.out.println("3. Acceptor: M4 - M9 has random response time: immediate; medium; late; never.");
         System.out.println("======================================================\n");
 
         Election test = new Election();
@@ -20,5 +21,11 @@ public class TesterMultiNormal extends Election {
         test.propose(M1);
         test.propose(M2);
         test.propose(M3);
+
+        // randomly choose M2 or M3 go offline after making proposal
+        test.goOffline();
+
+        //let M4-M9 have random response time
+        test.doRandom(false);
     }
 }
